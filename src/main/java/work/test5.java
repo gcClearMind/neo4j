6a1fe9bb9ca121ca72d91ac29c8235a5af02546e
shk314159 <50333579+shk314159@users.noreply.github.com>
2024-01-13 20:23:31 +0800

    寄
    ad

--- src/main/java/work/test5.java
+++ src/main/java/work/test5.java
@@ -3,12 +3,15 @@ package work;
 import org.apache.jena.assembler.RuleSet;
 import org.apache.jena.graph.Triple;
 import org.apache.jena.ontology.OntModel;
+import org.apache.jena.ontology.OntModelSpec;
+import org.apache.jena.ontology.ProfileRegistry;
 import org.apache.jena.rdf.model.Model;
 import org.apache.jena.rdf.model.ModelFactory;
 import org.apache.jena.rdf.model.StmtIterator;
 import org.apache.jena.reasoner.InfGraph;
 import org.apache.jena.reasoner.rulesys.GenericRuleReasoner;
 import org.apache.jena.reasoner.rulesys.GenericRuleReasonerFactory;
+import org.apache.jena.reasoner.rulesys.RDFSRuleReasonerFactory;
 import org.apache.jena.util.PrintUtil;
 import tool.CoreOWLUtil;
 
@@ -28,29 +31,29 @@ public class test5 {
 //        properties.load(inputStream);
 
         //rdf文件
-        Model model = ModelFactory.createDefaultModel();
+
         SetSourceName("http://www.neo4j.com/ontologies/data.owl");
 
         String inputFileName = Paths.get("test5.rdf").toString();
+        Model model = ModelFactory.createOntologyModel(OntModelSpec.OWL_MEM_TRANS_INF);
         OntModel ontModel = getOntModel(model, inputFileName);
         GenericRuleReasoner reasoner = (GenericRuleReasoner) GenericRuleReasonerFactory.theInstance().create(null);
         String ruleString1 = "a(?a), b(?b), r1(?a, ?b), d(?d), r4(?b, ?d) -> r5(?a, ?d)";
-        String ruleString2 = "[Rules: (?a rdf:type http://www.neo4j.com/ontologies/data.owl#a), " +
-                "(?a http://www.neo4j.com/ontologies/data.owl#r1 ?b), (?b rdf:type http://www.neo4j.com/ontologies/data.owl#b), " +
-                "(?b http://www.neo4j.com/ontologies/data.owl#r4 ?d), (?d rdf:type http://www.neo4j.com/ontologies/data.owl#d) -> " +
-                "(?a http://www.neo4j.com/ontologies/data.owl#r5 ?d)]";
+        String ruleString2 = "[Rules: (?a rdf:type http://www.neo4j.com/ontologies/data.owl#a), (?a http://www.neo4j.com/ontologies/data.owl#r1 ?b), (?b rdf:type http://www.neo4j.com/ontologies/data.owl#b), (?b http://www.neo4j.com/ontologies/data.owl#r4 ?d), (?d rdf:type http://www.neo4j.com/ontologies/data.owl#d) -> (?a http://www.neo4j.com/ontologies/data.owl#r5 ?d)]";
         RuleSet ruleSet = RuleSet.create(ruleString2);
         reasoner.setRules(ruleSet.getRules());
         reasoner.setMode(GenericRuleReasoner.HYBRID);
-        InfGraph infgraph = reasoner.bind(model.getGraph());
+        InfGraph infgraph = reasoner.bind(ontModel.getGraph());
 
         Iterator<Triple> tripleIterator = infgraph.getRawGraph().find(null, null, null);
-        PrintUtil.registerPrefix("", CoreOWLUtil.getNameSpace());
+//        PrintUtil.registerPrefix("", CoreOWLUtil.getNameSpace());
         while (tripleIterator.hasNext()) {
             System.out.println(tripleIterator.next());
         }
         model = ModelFactory.createModelForGraph(infgraph.getRawGraph());
-        OutputStream out = Files.newOutputStream(Paths.get("test5.rdf"));
+
+        OutputStream out = Files.newOutputStream(Paths.get("test6.rdf"));
         model.write(out,"RDF/XML-ABBREV");
+
     }
 }
