7ea87a7a64474a6282f1aa1f8eb951209f0e289e
shk001 <296155457@qq.com>
2024-10-29 17:14:05 +0800

    add file

--- src/main/java/work/test_find_roads.java
+++ src/main/java/work/test_find_roads.java
@@ -10,6 +10,8 @@ import org.neo4j.driver.Record;
 import tool.CoreOWLUtil;
 import tool.Path;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.file.Paths;
@@ -35,17 +37,29 @@ public class test_find_roads {
         String inputFileName = Paths.get("data/output_individual.rdf").toString();
         OntModel ontModel = getOntModel(model, inputFileName);
 
+        String filePath = "data/path/output.txt";
+
         String start = "Block";
         String end = "Block";
-        int pathLen = 4;
+        int pathLen = 6;
+        BufferedWriter writer = new BufferedWriter(new FileWriter(filePath));
         Set<String> SWRL_list = new HashSet<>();
         try (Session session = driver.session()) {
             Result result = session.run("match p = (n:`" + start + "`)-[*2.."+ pathLen +"]->(m:`" + end + "`) " +
                     "return p as list");
+            int id = 0;
             while (result.hasNext()) {
                 Record record = result.next();
                 org.neo4j.driver.types.Path path = record.get("list").asPath();
                 String swrl = CoreOWLUtil.getSWRL(ontModel, path);
+                writer.write(String.valueOf(++id));
+                writer.newLine();
+                writer.write(showPath(path));
+                writer.newLine();
+                writer.write(swrl);
+                writer.newLine();
+                writer.newLine();
+                writer.flush();
                 System.out.println("-------------------------------------------------------------------------------------------");
                 System.out.println(swrl);
                 System.out.println(showPath(path));
@@ -54,8 +68,13 @@ public class test_find_roads {
                 System.out.println();
                 SWRL_list.add(swrl);
             }
+            writer.write("--------------------------------------------------------------------------------------");
+            writer.newLine();
             for(String swrl : SWRL_list) {
+                writer.write(swrl);
+                writer.newLine();
                 System.out.println(swrl);
+                writer.flush();
             }
         }
         catch (Exception e) {
